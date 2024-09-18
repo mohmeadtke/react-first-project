1eb7c5b7afc989dec49a1f1d365ab8912b8f5db4
TAQI <mohmmead49@gmail.com>
2024-09-18 23:33:20 +0300

    intitial commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Homepage = () => {
+    return (  
+        <div className="home">
+            <p>helowwwww bithcess</p> 
+        </div>
+    );
+}
+ 
+export default Homepage;
---
+++ src/Navbarr.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>home</h1>
+            <div className="links">
+                <a href="/">home</a>
+                <a href="/create"> not hme</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
